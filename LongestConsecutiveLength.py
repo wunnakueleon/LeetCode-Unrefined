b8f3ece2e559a32f8726dcf492eedf8a14427f5a
Wunna <wannamoesan11@gmail.com>
2025-08-07 08:25:04 +0700

    Create LongestConsecutiveLength.py
    Longest Consecutive Length

---
+++ LongestConsecutiveLength.py
@@ -0,0 +1,15 @@
+def longest_consecutive_length(nums):
+    consecutive_list = []
+    sorted_list = sorted(nums)
+
+    consecutive_list.append(sorted_list[0])
+    for each_num in range(1, len(nums)):
+        if (sorted_list[each_num] - 1 == sorted_list[each_num - 1]) or (sorted_list[each_num] == sorted_list[each_num - 1]):
+            consecutive_list.append(sorted_list[each_num])
+
+
+    return consecutive_list
+
+
+input_list = [0, 1, 3, 2, 1, 90, 28, 19]
+print(len(longest_consecutive_length(input_list)))
